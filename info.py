902052d28cf392010f056a4259c9469e71a67a5a
sreethar1205 <727723euec513@skcet.ac.in>
2024-07-23 00:27:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,8 +22,7 @@ API_ID = int(environ.get('API_ID', '23635334'))
 API_HASH = environ.get('API_HASH', '15527b6a0e728acb464eb219291ef99c')
 BOT_TOKEN = environ.get('BOT_TOKEN', "Done! Congratulations on your new bot. You will find it at t.me/TNmoviesHub_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
 
-Use this token to access the HTTP API:
-6732132534:AAEm5qt1buUZ6rNXZc_uUEzPE9UEN_8PJFA
+Use this token to access the HTTP API:6732132534:AAEm5qt1buUZ6rNXZc_uUEzPE9UEN_8PJFA
 Keep your token secure and store it safely, it can be used by anyone to control your bot.
 
 For a description of the Bot API, see this page: https://core.telegram.org/bots/api")
